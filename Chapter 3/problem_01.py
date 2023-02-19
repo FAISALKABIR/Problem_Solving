50f7a8dc362020db30469b09b4f2cfadb7d961db
FAISALKABIR <faisalkabir1993@gmail.com>
2023-02-19 12:51:10 +0600

    chapter 3

---
+++ Chapter 3/problem_01.py
@@ -0,0 +1,18 @@
+"""
+Check whether a given number is a palindrome or not
+"""
+
+num = int(input("Enter a number: "))
+
+temp = num
+rev = 0
+
+while (num > 0):
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10
+
+if (temp == rev):
+    print("The given number is Palindrome..!")
+else:
+    print("The given number is not a Palindrome..!")
