1886fd273cb4632e8eff3f4513ceff7436c27ec0
FAISALKABIR <faisalkabir1993@gmail.com>
2023-02-23 21:55:21 +0600

    problem 4 solved

--- Programming Contest_Data Structure & Algorithm Book (Exercises)/Chapter 4 (loop)/problem_04.py
+++ Programming Contest_Data Structure & Algorithm Book (Exercises)/Chapter 4 (loop)/problem_04.py
@@ -5,9 +5,14 @@ Find the sum of the series: 1 + (2+3) + (4+5+6) + .... + nth term
 n = int(input('Enter the range of the number: '))
 
 sum = 0
+start = 1
+end = 1
 
 for i in range(1, n+1):
-    for j in range(i+1, i):
+    for j in range(start, end+1):
         sum += j
+
+    start = end + 1
+    end = start + i
 
 print("The sum of the series: ", sum)
