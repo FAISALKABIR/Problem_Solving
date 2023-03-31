2fa5925799f78e672c4b53271603f762d6ea517a
FAISALKABIR <faisalkabir1993@gmail.com>
2023-03-31 17:26:53 +0600

    P 40 complete

---
+++ 100+ Python Exercises/Level 1 (Beginner)/Problem 23.py
@@ -0,0 +1,14 @@
+"""
+Question 23:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+"""
+
+# Solution:
+def square(num):
+    return num ** 2
+
+print(square(2))
+print(square(3))
